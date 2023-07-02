05f0124e201f825f350e83d9237eecc0ae8b388d
GAJAWADA VARSHITH KUMAR <79400826+Varshithkumar26@users.noreply.github.com>
2023-07-02 21:38:27 +0530

    Create Tree Traversals.cpp

---
+++ Tree Traversals.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h> 
+/************************************************************
+
+    Following is the Binary Tree node structure:
+
+    class BinaryTreeNode
+    {
+    public :
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+
+************************************************************/
+
+vector<vector<int>> getTreeTraversal(BinaryTreeNode<int> *root){
+    // Write your code here.
+    vector<vector<int>> ans;
+    if(!root)
+      return ans;
+    vector<int> preorder;
+    vector<int> inorder;
+    vector<int> postorder;
+    stack<pair<BinaryTreeNode<int> *,int>> st;
+    st.push({root,1});
+    while(!st.empty())
+    {
+        BinaryTreeNode<int> *node=st.top().first;
+        int num=st.top().second;
+        st.pop();
+        if(num==1)
+        {
+            preorder.push_back(node->data);
+            st.push({node,num+1});
+            if(node->left)
+              st.push({node->left,1});
+        }
+        else if(num==2)
+        {
+            inorder.push_back(node->data);
+            st.push({node,num+1});
+            if(node->right)
+              st.push({node->right,1});
+        }
+        else
+        {
+            postorder.push_back(node->data);
+        }
+    }
+
+    ans.push_back(inorder);
+    ans.push_back(preorder);
+    ans.push_back(postorder);
+    return ans;
+}
