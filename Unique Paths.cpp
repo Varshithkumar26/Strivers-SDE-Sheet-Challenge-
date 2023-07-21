0d832a679f2ed027bb54a26632b06c925006c39c
GAJAWADA VARSHITH KUMAR <79400826+Varshithkumar26@users.noreply.github.com>
2023-07-21 17:32:01 +0530

    Update Unique Paths.cpp

--- Unique Paths.cpp
+++ Unique Paths.cpp
@@ -1,14 +1,14 @@
-#include <bits/stdc++.h>
-
+#include <bits/stdc++.h> 
 int uniquePaths(int m, int n) {
-  // Write your code here.
+	// Write your code here.
+	
+	// res=(m+n-2)C(m-1)
+	int res=1;
+	for(int i=1;i<m;i++)
+	{
+		res*=(m+n-1-i);
+		res=res/i;
+	}
 
-  // ans=(m+n-2)C(n-1)
-  int a = m + n - 2;
-  int b = n - 1;
-  long long ans = 1;
-  for (int i = 1; i <= b; i++)
-     ans=ans*(a-b+i)/i;
-  
-  return (int)ans;
+	return res;
 }
